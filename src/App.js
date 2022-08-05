5fdd3a9dbbd840f7058764c92cb17f3edd844dc7
Ramon souza <ramonkyo@gmail.com>
2022-08-05 14:12:17 -0400

    feat: Api added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import TopButtons from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureDetails from "./components/TemperatureDetails";
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/Api";
 
 
 
 function App() {
+
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: 'toronto' })
+    console.log(data)
+  }
+
+  fetchWeather()
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
@@ -15,6 +25,9 @@ function App() {
 
       <TimeAndLocation />
       <TemperatureDetails />
+
+      <Forecast title='Hourly forecast'/>
+      <Forecast title='daily forecast'/>
     </div>
 
   );
